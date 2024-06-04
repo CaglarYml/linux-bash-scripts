ab3e09edcfab4ca60e079f3717043396ad49ac84
Çağlar <caglar.yml@gmail.com>
2024-06-04 23:47:57 +0300

    Create FirewallOpen.sh

---
+++ firewall-scripts/FirewallOpen.sh
@@ -0,0 +1,6 @@
+echo Bu betik CaglarYml tarafindan kullanilmasi adina
+echo hazirlanmistir. Uncompiled Firewall (UFW)
+echo kullanilmistir.
+ufw allow 22
+yes | sudo ufw enable
+echo Uncompiled Firewall (UFW) aktif olmustur.
