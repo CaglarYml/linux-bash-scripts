e110fe59c496a557ed710c1e53923a960f938c73
Çağlar <caglar.yml@gmail.com>
2024-06-05 21:52:52 +0300

    Create FirewallDisable.sh

---
+++ firewall-scripts/FirewallDisable.sh
@@ -0,0 +1,5 @@
+echo Bu betik CaglarYml tarafindan kullanilmasi adina
+echo hazirlanmistir. Uncompiled Firewall (UFW)
+echo kullanilmistir.
+sudo ufw disable
+echo Uncompiled Firewall (UFW) devre disi birakilmistir.
