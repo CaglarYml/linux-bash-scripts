328a19cdd12dd7f3155b519ded53bd7984472f18
Çağlar <caglar.yml@gmail.com>
2024-06-12 09:34:04 +0300

    Create jdk21-download.sh

---
+++ java-download-scripts/jdk21-download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo ----------------------------------------------------------------------------
+echo Bu betik CaglarYml tarafindan kullanilmasi adina hazirlanmistir.
+echo ----------------------------------------------------------------------------
+apt install -y wget apt-transport-https gpg
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+apt update
+apt install temurin-21-jdk
+echo ----------------------------------------------------------------------------
+echo JDK 21 KURULMUSTUR
+echo ----------------------------------------------------------------------------
